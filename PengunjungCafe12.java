85666fb793ba76a4c7ceaa08d6fdd9f83694bbe7
kaabiiii <kaabimrfuad1@gmail.com>
2024-11-23 18:24:12 +0700

    Fungsi 1

---
+++ PengunjungCafe12.java
@@ -0,0 +1,12 @@
+public class PengunjungCafe12 {
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+    }
+
+    public static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung:");
+        for (String nama : namaPengunjung) { 
+            System.out.println("- " + nama);
+        }
+    }
+}
